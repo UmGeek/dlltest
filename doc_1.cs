3b2a51c69e46044c835eb8f9ff8c4e5968a4a3b9
Um Geek <36805078+UmGeek@users.noreply.github.com>
2018-03-20 23:47:02 -0300

    Add files via upload

---
+++ doc_1.cs
@@ -0,0 +1,18 @@
+﻿/* Created by SharpDevelop. */
+
+using System;
+using RGiesecke.DllExport;
+using System.Runtime.InteropServices;
+
+namespace MeuTeste
+{
+   internal static class dllExportTest
+   {
+     [DllExport("fteste", CallingConvention = CallingConvention.StdCall)]
+     static bool fteste()
+     {
+         return true;
+     }
+   }   
+}
+
